483f549ff9f39e8d8b7243ca7ffe2738a1117484
splaza28 <global-splaza28@gmail.com>
2022-01-16 18:39:21 -0600

    created database and tables

---
+++ db/db.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE directory;
+USE directory;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO staff (first_name, last_name, role_id, manage_id)
+VALUES
+  ('Virginia', 'Woolf', 2, 5),
+  ('Ronald', 'Firbank', 1, 3);
