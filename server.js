83e05802d825a455d36f5ec176be9c90a69be783
splaza28 <global-splaza28@gmail.com>
2022-01-17 23:16:50 -0600

    moved connection to separate file

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mysql = require('mysql2');
+const db = require('./db/connection');
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // Your MySQL username,
 
-      user: 'root',
-      // Your MySQL password
-      password: 'Boymom1921!!',
-      //update database name when created
-      database: 'directory'
-    },
-    console.log('Connected to the directory database.')
-  );
+
+  app.get('/api/staff', (req, res) => {
+    const sql = `SELECT * FROM staff`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+  
 
   app.get('/api/staff/:id', (req, res) => {
     const sql = `SELECT * FROM staff WHERE id = ?`;
@@ -60,42 +65,55 @@ const db = mysql.createConnection(
 //     console.log(result);
 //   });
 
-app.delete('/api/staff/:id', (req, res) => {
-    const sql = `DELETE FROM staff WHERE id = ?`;
-    const params = [req.params.id];
+// app.delete('/api/staff/:id', (req, res) => {
+//     const sql = `DELETE FROM staff WHERE id = ?`;
+//     const params = [req.params.id];
+  
+//     db.query(sql, params, (err, result) => {
+//       if (err) {
+//         res.statusMessage(400).json({ error: res.message });
+//       } else if (!result.affectedRows) {
+//         res.json({
+//           message: 'Employee not found'
+//         });
+//       } else {
+//         res.json({
+//           message: 'deleted',
+//           changes: result.affectedRows,
+//           id: req.params.id
+//         });
+//       }
+//     });
+//   });
+
+app.post('/api/staff', ({ body }, res) => {
+    // const errors = inputCheck(
+    //   body,
+    //   'first_name',
+    //   'last_name',
+    //   'industry_connected'
+    // );
+    // if (errors) {
+    //   res.status(400).json({ error: errors });
+    //   return;
+    // }
+  
+    const sql = `INSERT INTO staff (first_name, last_name, role_id, manage_id)
+      VALUES (?,?,?,?)`;
+    const params = [body.first_name, body.last_name, body.role_id, body.manage_id];
   
     db.query(sql, params, (err, result) => {
       if (err) {
-        res.statusMessage(400).json({ error: res.message });
-      } else if (!result.affectedRows) {
-        res.json({
-          message: 'Employee not found'
-        });
-      } else {
-        res.json({
-          message: 'deleted',
-          changes: result.affectedRows,
-          id: req.params.id
-        });
+        res.status(400).json({ error: err.message });
+        return;
       }
+      res.json({
+        message: 'success',
+        data: body
+      });
     });
   });
-
-  app.post('/api/staff', ({ body }, res) => {
-    const sql = `INSERT INTO staff (first_name, last_name, role_id, manage_id)
-    VALUES (?,?,?,?)`;
-  const params = [body.first_name, body.last_name, body.role_id, body.manage_id];
   
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: body
-    });
-  });
 
   app.use((req, res) => {
     res.status(404).end();
@@ -105,4 +123,3 @@ app.delete('/api/staff/:id', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-});
